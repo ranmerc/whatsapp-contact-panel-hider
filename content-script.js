6e191a780168e15304dbc41b3774b675ca6228b9
Kamran Ansari <ranmerc@outlook.com>
2024-08-10 20:14:17 +0530

    fix: update for new layout

--- content-script.js
+++ content-script.js
@@ -23,15 +23,16 @@ const addVisibilityIcon = async () => {
   const showSVG = `<svg xmlns="http://www.w3.org/2000/svg" aria-label="Show Side Panel" width="1.8em" height="1.8em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="var(--panel-header-icon)" d="M12 5c-7.633 0-9.927 6.617-9.948 6.684L1.946 12l.105.316C2.073 12.383 4.367 19 12 19s9.927-6.617 9.948-6.684l.106-.316l-.105-.316C21.927 11.617 19.633 5 12 5zm0 11c-2.206 0-4-1.794-4-4s1.794-4 4-4s4 1.794 4 4s-1.794 4-4 4z"/><path fill="var(--panel-header-icon)" d="M12 10c-1.084 0-2 .916-2 2s.916 2 2 2s2-.916 2-2s-.916-2-2-2z"/></svg>`;
   const hideSVG = `<svg xmlns="http://www.w3.org/2000/svg" aria-label="Hide Side Panel" width="1.8em" height="1.8em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="var(--panel-header-icon)" d="M8.073 12.194L4.212 8.333c-1.52 1.657-2.096 3.317-2.106 3.351L2 12l.105.316C2.127 12.383 4.421 19 12.054 19c.929 0 1.775-.102 2.552-.273l-2.746-2.746a3.987 3.987 0 0 1-3.787-3.787zM12.054 5c-1.855 0-3.375.404-4.642.998L3.707 2.293L2.293 3.707l18 18l1.414-1.414l-3.298-3.298c2.638-1.953 3.579-4.637 3.593-4.679l.105-.316l-.105-.316C21.98 11.617 19.687 5 12.054 5zm1.906 7.546c.187-.677.028-1.439-.492-1.96s-1.283-.679-1.96-.492L10 8.586A3.955 3.955 0 0 1 12.054 8c2.206 0 4 1.794 4 4a3.94 3.94 0 0 1-.587 2.053l-1.507-1.507z"/></svg>`;
 
-  const iconHeader = await waitForElm(
-    `header  > div:nth-of-type(2) > div > span`
+  const lastIconInSideHeader = await waitForElm(
+    `header > div  div:has(>div[aria-label="Meta AI"])`
   );
 
   const iconButton = document.createElement('button');
   iconButton.type = 'button';
-  iconButton.style.marginInline = '18px';
+  iconButton.style.marginBlockStart = '18px';
+  iconButton.style.marginInlineStart = '8px';
   iconButton.innerHTML = hideSVG;
-  iconHeader.prepend(iconButton);
+  lastIconInSideHeader.insertAdjacentElement('afterend', iconButton);
 
   const sidePanel = await waitForElm(`#side`);
 
@@ -79,7 +80,8 @@ const addPanelIcon = async () => {
 
   const sidePanel = await waitForElm('#side');
   const panel = sidePanel.parentElement;
-  const headPanel = await waitForElm('header');
+  const sideHeadPanel = await waitForElm('header');
+  const topHeadPanel = await waitForElm('header:has(h1)');
   panel.style.transition = 'max-width 150ms ease-in';
 
   panel.prepend(iconContainer);
@@ -87,12 +89,14 @@ const addPanelIcon = async () => {
   iconButton.addEventListener('click', () => {
     if (!isClosed) {
       panel.style.maxWidth = '0';
-      headPanel.style.display = 'none';
+      sideHeadPanel.style.display = 'none';
+      topHeadPanel.style.display = 'none';
       iconButton.innerHTML = showPanelSVG;
       isClosed = true;
     } else {
       panel.style.removeProperty('max-width');
-      headPanel.style.removeProperty('display');
+      sideHeadPanel.style.removeProperty('display');
+      topHeadPanel.style.removeProperty('display');
       iconButton.innerHTML = hidePanelSVG;
       isClosed = false;
     }
